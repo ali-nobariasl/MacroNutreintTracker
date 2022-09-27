e06576b6f23dc9a2ce11c357146082847850d6b0
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-09-27 09:47:16 +0300

    addindDeletePageAndView

--- Macro/Macro/urls.py
+++ Macro/Macro/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from myapp.views import index
+from myapp.views import index, delete_consume
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index , name='index'),
+    path('delete/<int:id>/',delete_consume, name='delete' ),
 ]
